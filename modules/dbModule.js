deb05212976f72b3b3d86ea73c6aea89a3b07276
unknown <dbwjd3508@lotte.net>
2018-10-25 15:54:53 +0900

    (2018-10-25) add by juj: set timeout in mongodb connection

--- modules/dbModule.js
+++ modules/dbModule.js
@@ -1,12 +1,19 @@
 const mongoose = require('mongoose');
-const config = require('../config')
+const config = require('../config');
 
+
+const option = {
+    socketTimeoutMS: 30000,
+    keepAlive: true,
+    reconnectTries: 30000,
+    useNewUrlParser: true
+};
 
 const DBModule = (function(){
     return {
         Init: function(){
             (async () => {
-                mongoose.connect(config.mongoose.url, { useNewUrlParser: true});
+                mongoose.connect(config.mongoose.url, option);
             })().then(_ => {
                 const db = mongoose.connection;
                 db.on('error', console.error);
