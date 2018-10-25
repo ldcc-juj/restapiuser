264f13a664ed92db1b06573bbb915ebf8e9bbf96
unknown <dbwjd3508@lotte.net>
2018-10-25 14:51:14 +0900

    (2018-10-25) add by juj: add ec2 config

---
+++ config/env/ec2.js
@@ -0,0 +1,14 @@
+module.exports = {
+    server: {
+        port: 8081
+    },
+    redis: {
+        redisHost: "",
+        redisPort: 6379,
+        redisDatabase: 1,
+        redisPassword: null
+    },
+    mongoose: {
+        url: 'mongodb://10.131.144.132/test'
+    }
+};
--- config/index.js
+++ config/index.js
@@ -1,4 +1,5 @@
 const development = require('./env/development');
+const ec2 = require('./env/ec2');
 const path = require('path');
 
 const defaults = {
@@ -6,5 +7,6 @@ const defaults = {
 };
 
 module.exports = {
-    development: Object.assign({}, development, defaults)
+    development: Object.assign({}, development, defaults),
+    ec2: Object.assign({}, ec2, defaults),
 }[process.env.NODE_ENV || 'development']
